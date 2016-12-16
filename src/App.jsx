75dca6f5ed12bcc0a8e723687343095b8372c164
Taufiq Hoven <taufiq.hoven@gmail.com>
2016-12-16 20:48:54 +1000

    Add input

--- src/App.jsx
+++ src/App.jsx
@@ -22,10 +22,21 @@ const list = [
 ];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        { list.map(item =>
+        <form>
+          <input type="text" />
+        </form>
+        { this.state.list.map(item =>
           <div key={item.objectID}>
             <span><a href={item.url}>{item.title}</a></span>
             <span>{item.author}</span>
