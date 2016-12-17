8e8036ecbe742ceb98058a1ffcc3be5a755db732
Taufiq Hoven <taufiq.hoven@gmail.com>
2016-12-17 22:02:20 +1000

    Add Hacker news fetch

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,12 @@ const REPO_LIST = [
   },
 ];
 
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
 function isSearched(query) {
   return function isQuerySearchedOn(item) {
     return !query || item.title.toLowerCase().indexOf(query.toLowerCase()) !== -1;
@@ -34,15 +40,27 @@ const Search = ({ value, onChange, children }) =>
     </form>
   );
 
+const largeColumn = { width: '40%' };
+const midColumn = { width: '30%' };
+const smallColumn = { width: '15%' };
+
 const Table = ({ list, pattern }) =>
   (
-    <div>
+    <div className="table">
       { list.filter(isSearched(pattern)).map(item =>
-        <div key={item.objectID}>
-          <span><a href={item.url}>{item.title}</a></span>
-          <span>{item.author}</span>
-          <span>{item.numComments}</span>
-          <span>{item.points}</span>
+        <div key={item.objectID} className="table-row">
+          <span style={largeColumn}>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span style={midColumn}>
+            {item.author}
+          </span>
+          <span style={smallColumn}>
+            {item.numComments}
+          </span>
+          <span style={smallColumn}>
+            {item.points}
+          </span>
         </div>,
       )}
     </div>
@@ -53,32 +71,46 @@ class App extends Component {
     super(props);
 
     this.state = {
+      result: null,
       list: REPO_LIST,
-      query: '',
+      query: DEFAULT_QUERY,
     };
 
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  componentDidMount() {
+    const { query } = this.state;
+    this.fetchSearchTopStories(query);
   }
 
   onSearchChange(event) {
     this.setState({ query: event.target.value });
   }
 
+  setSearchTopStories(result) {
+    this.setState({ result });
+  }
+
+  fetchSearchTopStories(query) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${query}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result));
+  }
+
   render() {
-    const { query, list } = this.state;
+    const { query, result } = this.state;
 
     return (
-      <div className="App">
-        <Search value={query} onChange={this.onSearchChange}>
-          Search
-        </Search>
-        <Table list={list} pattern={query} />
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className="page">
+        <div className="interactions">
+          <Search value={query} onChange={this.onSearchChange}>
+            Search
+          </Search>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        { result ? <Table list={result.hits} pattern={query} /> : null }
       </div>
     );
   }
